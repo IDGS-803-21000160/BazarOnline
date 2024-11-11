3fddda58732403e307289d8d4ef6ddf94c9cffeb
Ivan Blancarte <iblancarte583@gmail.com>
2024-11-11 00:15:34 -0600

    feat: cambios del diseño

--- src/components/DetailComplete.jsx
+++ src/components/DetailComplete.jsx
@@ -15,32 +15,67 @@ const DetailComplete = ({ product }) => {
           {product.title}
         </h1>
         <img
-          className="w-full h-64 object-cover mb-4"
+          className="w-full h-64 object-cover mb-4 border-4 border-gray-200 rounded-lg"
           src={product.thumbnail}
           alt={product.title}
         />
-        <p className="text-gray-700 dark:text-gray-300 mb-2">
+        <p className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-white">
           {product.description}
         </p>
-        <p className="text-xl font-semibold text-gray-900 dark:text-white mb-2">
-          Precio: ${product.price}
-        </p>
-        <p className="text-lg text-gray-700 dark:text-gray-300 mb-2">
-          Descuento: {product.discountPercentage}%
-        </p>
-        <p className="text-lg text-gray-700 dark:text-gray-300 mb-2">
-          Rating: {product.rating}
-        </p>
-        <p className="text-lg text-gray-700 dark:text-gray-300 mb-2">
-          Stock: {product.stock}
-        </p>
-        <p className="text-lg text-gray-700 dark:text-gray-300 mb-2">
-          Marca: {product.brand}
-        </p>
-        <p className="text-lg text-gray-700 dark:text-gray-300 mb-4">
-          Categoría: {product.category}
-        </p>
-        <div>
+        <table className="min-w-full bg-white dark:bg-gray-800">
+          <tbody>
+            <tr className="border-b dark:border-gray-700"></tr>
+            <tr className="border-b dark:border-gray-700">
+              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-white">
+                Precio
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700 dark:text-gray-300">
+                ${product.price}
+              </td>
+            </tr>
+            <tr className="border-b dark:border-gray-700">
+              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-white">
+                Descuento
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700 dark:text-gray-300">
+                {product.discountPercentage}%
+              </td>
+            </tr>
+            <tr className="border-b dark:border-gray-700">
+              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-white">
+                Rating
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700 dark:text-gray-300">
+                {product.rating}
+              </td>
+            </tr>
+            <tr className="border-b dark:border-gray-700">
+              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-white">
+                Stock
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700 dark:text-gray-300">
+                {product.stock}
+              </td>
+            </tr>
+            <tr className="border-b dark:border-gray-700">
+              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-white">
+                Marca
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700 dark:text-gray-300">
+                {product.brand}
+              </td>
+            </tr>
+            <tr className="border-b dark:border-gray-700">
+              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-white">
+                Categoría
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700 dark:text-gray-300">
+                {product.category}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="mt-6">
           <h2 className="text-2xl font-semibold text-gray-900 dark:text-white mb-4">
             Imágenes
           </h2>
@@ -57,7 +92,7 @@ const DetailComplete = ({ product }) => {
         </div>
         <button
           onClick={handleAddToCart}
-          className="mt-4 text-white w-full bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-800"
+          className="mt-4 w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-800"
         >
           Agregar al Carrito
         </button>
